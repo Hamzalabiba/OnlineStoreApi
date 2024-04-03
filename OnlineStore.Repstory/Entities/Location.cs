642f131404db8c14d347c96da98711c60887fdea
hamza alahmad <hamza.a.alahmad@outlook.com>
2024-04-03 09:33:27 +0300

    d

---
+++ OnlineStore.Repstory/Entities/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.Domain.Entities
+{
+    public class Location
+    {
+        public int StreetNo { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public int BuldingNo { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
---
+++ OnlineStore.Repstory/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineStore.Domain.Entities
+{
+    public class Product 
+    {
+        public string Name { get; set; }
+        public decimal price { get; set; }
+    }
+}
