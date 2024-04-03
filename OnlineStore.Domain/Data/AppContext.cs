f49bc27bf742b092ebd58ddaf231b8b832a047b1
hamza alahmad <hamza.a.alahmad@outlook.com>
2024-04-03 08:09:53 +0300

    Add AppContext

---
+++ OnlineStore.Domain/Data/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Domain.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<UserType> UserTypes { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }      
+}
