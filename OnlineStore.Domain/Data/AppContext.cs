6a895552932e325209e8426257e3441d7730166f
hamza alahmad <hamza.a.alahmad@outlook.com>
2024-04-03 08:54:28 +0300

    Add Data Base

--- OnlineStore.Domain/Data/AppContext.cs
+++ OnlineStore.Domain/Data/AppContext.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace OnlineStore.Domain.Data
 {
-    public class AppContext : DbContext
+    public class ApplicationContext : DbContext
     {
-        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
