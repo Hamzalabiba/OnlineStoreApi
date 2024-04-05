82e1a2ddf73f5a89deffab43b7dd3dbfd631571b
hamza alahmad <hamza.a.alahmad@outlook.com>
2024-04-05 08:45:45 +0300

    Add Category And products controllers

---
+++ OnlineStoreApi/Controllers/Prodact/CategoryController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Service.CustomServices;
+
+namespace OnlineStoreApi.Controllers.Prodact
+{
+    [Route("Api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICustomService<Category> _customService;
+
+        public CategoryController(ICustomService<Category> customService)
+        {
+            _customService = customService;
+        }
+
+        [HttpGet(nameof(GetCategory))]
+        public IActionResult GetCategory()
+        {
+            List<Category> Category = new List<Category>();
+            Category = _customService.GetAll().ToList();
+            if (Category is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Category);
+            }
+        }
+
+        [HttpGet(nameof(GetProductById))]
+        public IActionResult GetProductById(int id)
+        {
+            Category user = new Category();
+            user = _customService.GetById(id);
+            if (user is null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost(nameof(PostInsertProduct))]
+        public IActionResult PostInsertProduct(Category user)
+        {
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _customService.Insert(user);
+                return Ok();
+            }
+        }
+
+        [HttpPost(nameof(PostDeleteProduct))]
+        public IActionResult PostDeleteProduct(Category user)
+        {
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _customService.Delete(user);
+                return Ok();
+            }
+        }
+
+        [HttpPost(nameof(PostUpdateProduct))]
+        public IActionResult PostUpdateProduct(Category user)
+        {
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _customService.Update(user);
+                return Ok();
+            }
+        }
+    }
+}
---
+++ OnlineStoreApi/Controllers/Prodact/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Service.CustomServices;
+
+namespace OnlineStoreApi.Controllers.Prodact
+{
+    [Route("Api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly ICustomService<Products> _customService;
+
+        public ProductController(ICustomService<Products> customService)
+        {
+            _customService = customService;
+        }
+
+        [HttpGet(nameof(GetProducts))]
+        public IActionResult GetProducts()
+        {
+            List<Products> Products = new List<Products>();
+            Products = _customService.GetAll().ToList();
+            if (Products is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Products);
+            }
+        }
+
+        [HttpGet(nameof(GetProductById))]
+        public IActionResult GetProductById(int id)
+        {
+            Products user = new Products();
+            user = _customService.GetById(id);
+            if (user is null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost(nameof(PostInsertProduct))]
+        public IActionResult PostInsertProduct(Products user)
+        {
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _customService.Insert(user);
+                return Ok();
+            }
+        }
+
+        [HttpPost(nameof(PostDeleteProduct))]
+        public IActionResult PostDeleteProduct(Products user)
+        {
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _customService.Delete(user);
+                return Ok();
+            }
+        }
+
+        [HttpPost(nameof(PostUpdateProduct))]
+        public IActionResult PostUpdateProduct(Products user)
+        {
+            if (user is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _customService.Update(user);
+                return Ok();
+            }
+        }
+    }
+}
